7ec5c0b86482bde901c8994ccf501301b1aec74d
julio@theowl.solutions <julio@theowl.solutions>
2022-07-14 18:55:45 -0500

    Add: store variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,28 @@
 <script>
   import MainLayout from "./components/organisms/layouts/mainLayout.svelte";
   import Main from "./components/organisms/main/main.svelte";
+  import projectsDefault from "./mockups/projects.mockups";
+
+  let projects = projectsDefault;
+
+  let selectedProject = projects[0];
+  let selectedVariablesGroup = selectedProject.variablesGroup[0];
+
+  function handleSelectProject(event) {
+    alert(event.detail.project.name);
+  }
+  function handleSelectVariablesGroup(event) {
+    alert(event.detail.variablesGroup.name);
+  }
 </script>
 
 <div class="container">
-  <MainLayout />
-  <Main />
+  <MainLayout
+    {projects}
+    on:selectProject={handleSelectProject}
+    on:selectVariablesGroup={handleSelectVariablesGroup}
+  />
+  <Main {selectedProject} {selectedVariablesGroup} />
 </div>
 
 <style>
