bbf4cc5413b57d863af541138d2a6ab3e84f0f80
julio@theowl.solutions <julio@theowl.solutions>
2022-07-14 18:56:00 -0500

    Add: projects mockups

---
+++ src/mockups/projects.mockups.js
@@ -0,0 +1,22 @@
+const projectsDefault = [
+  {
+    name: "Portal Project",
+    variablesGroup: [
+      {
+        name: "Owl Solutions variables",
+      },
+      {
+        name: "Mercado variables",
+      },
+    ],
+  },
+  {
+    name: "Admin Console Project",
+    variablesGroup: [
+      {
+        name: "Owl Solutions variables",
+      },
+    ],
+  },
+];
+export default projectsDefault;
