07d78096fd115dbeee3676b64c217789ad56eaeb
julio@theowl.solutions <julio@theowl.solutions>
2022-07-14 18:56:21 -0500

    Update: UI with information

--- src/components/organisms/layouts/mainLayout.svelte
+++ src/components/organisms/layouts/mainLayout.svelte
@@ -1,5 +1,45 @@
+<script>
+  export let projects;
+
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function selectProject(project) {
+    dispatch("selectProject", { project });
+  }
+  function selectVariablesGroup(variablesGroup, project) {
+    dispatch("selectVariablesGroup", { variablesGroup, project });
+  }
+</script>
+
 <div class="sidebar">
-  <p>hola mundo</p>
+  <h2>Eenvee</h2>
+  <ul>
+    <li>
+      Projects
+      <button>Add</button>
+      <ul>
+        {#each projects as project}
+          <li on:click={selectProject(project)}>
+            {project.name} <button>Add</button>
+            <ul>
+              {#each project.variablesGroup as variableGroup}
+                <li
+                  on:click|capture|stopPropagation={selectVariablesGroup(
+                    variableGroup,
+                    project
+                  )}
+                >
+                  {variableGroup.name}
+                </li>
+              {/each}
+            </ul>
+          </li>
+        {/each}
+      </ul>
+    </li>
+  </ul>
 </div>
 
 <style>
--- src/components/organisms/main/main.svelte
+++ src/components/organisms/main/main.svelte
@@ -1,11 +1,35 @@
+<script>
+  import InputsGroup from "../inputsGroup/inputsGroup.svelte";
+  import TextEditor from "../textEditor/textEditor.svelte";
+
+  export let selectedProject;
+  export let selectedVariablesGroup;
+
+  let selectedTypeOfView = "AsInputs";
+  const selectViewAsInputs = () => (selectedTypeOfView = "AsInputs");
+  const selectViewAsText = () => (selectedTypeOfView = "AsText");
+</script>
+
 <div class="main">
-  <p>hola mundo</p>
+  <h3>{selectedProject.name}</h3>
+  <h4>{selectedVariablesGroup.name}</h4>
+  <div>
+    <button on:click={selectViewAsInputs}>Show As Inputs</button>
+    <button on:click={selectViewAsText}>Show As Text</button>
+  </div>
+
+  {#if selectedTypeOfView === "AsInputs"}
+    <div>As Inputs</div>
+    <InputsGroup />
+  {:else}
+    <div>As texts</div>
+    <TextEditor />
+  {/if}
 </div>
 
 <style>
   .main {
     min-height: 100vh;
     width: calc(100vw - 300px);
-    background-color: orange;
   }
 </style>
