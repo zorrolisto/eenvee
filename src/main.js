72b9b8aee032dccbc9f84fa5b994fcbe7146dfe8
julio@theowl.solutions <julio@theowl.solutions>
2022-07-14 21:45:12 -0500

    Add: equals to array prototype

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import App from "./App.svelte";
 
+Array.prototype.equals = function (array) {
+  if (!array) return false;
+  if (this.length != array.length) return false;
+  for (var i = 0, l = this.length; i < l; i++) {
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      if (!this[i].equals(array[i])) return false;
+    } else if (this[i] != array[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+Object.defineProperty(Array.prototype, "equals", { enumerable: false });
+
 const app = new App({ target: document.body });
 export default app;
