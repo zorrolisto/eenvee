53a53736ac2a667d457badd04a9309de7f962756
julio@theowl.solutions <julio@theowl.solutions>
2022-07-14 21:45:58 -0500

    Add: more data to mockups

--- src/mockups/projects.mockups.js
+++ src/mockups/projects.mockups.js
@@ -4,9 +4,19 @@ const projectsDefault = [
     variablesGroup: [
       {
         name: "Owl Solutions variables",
+        variables: [
+          { field: "BD_URL", value: "db:3665?password=123&user=admin" },
+          { field: "NEXT_AUTH_URL", value: "http://localhost:3000" },
+          { field: "GOOGLE_SECRET", value: "9J$4F5DI2)JXS4HE353J" },
+        ],
       },
       {
         name: "Mercado variables",
+        variables: [
+          { field: "BD_URL", value: "db:3665?password=123&user=admin&MERCADO" },
+          { field: "NEXT_AUTH_URL", value: "http://localhost:3000/MERCADO" },
+          { field: "GOOGLE_SECRET", value: "9J$4F5DI2)JXS4HE353/MERCADO" },
+        ],
       },
     ],
   },
@@ -15,6 +25,11 @@ const projectsDefault = [
     variablesGroup: [
       {
         name: "Owl Solutions variables",
+        variables: [
+          { field: "BD_URL", value: "db:3665?password=123&user=admin" },
+          { field: "NEXT_AUTH_URL", value: "http://localhost:3000" },
+          { field: "GOOGLE_SECRET", value: "9J$4F5DI2)JXS4HE353J" },
+        ],
       },
     ],
   },
